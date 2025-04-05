d50cf780be8d8c89f749d8d3432b230cf388521f
mos9527 <greats3an@gmail.com>
2025-04-05 19:00:56 +0800

    Add Typetree info

---
+++ disco-vo.py
@@ -0,0 +1,59 @@
+import os, argparse, json
+import UnityPy
+from UnityPy.helpers.TypeTreeGenerator import TypeTreeGenerator
+from UnityPy.enums import ClassIDType
+from UnityPy.classes import MonoBehaviour
+
+MAX_DIALOGUE = 10000
+
+
+def __main__():
+    parser = argparse.ArgumentParser(description="Process Disco Elysium dialogue files")
+    parser.add_argument(
+        "--root",
+        default=r"C:\Program Files (x86)\Steam\steamapps\common\Disco Elysium",
+        help="Root directory of Disco Elysium installation",
+    )
+    parser.add_argument(
+        "--articy-map",
+        default="articy_mapping.json",
+        help="*INPUT* Articy ID:ConvoID.DialogueID mapping json",
+    )
+    parser.add_argument("output", help="Output ConvoID.DialogueID:VO Info json")
+    args = parser.parse_args()
+    print("Parsing Typetree info")
+    from uttcg import UTTCGen_Reread
+    from uttcg.VOTool import VoiceOverClipsLibrary
+
+    print("Loading data")
+    env = UnityPy.load(os.path.join(args.root, "disco_Data", "sharedassets1.assets"))
+
+    mono = [
+        obj.read(check_read=False)
+        for obj in env.objects
+        if obj.class_id == ClassIDType.MonoBehaviour
+    ]
+    vo_lib = next(filter(lambda x: x.m_Name == "VoiceOverClipsLibrary", mono))
+    vo_lib, clazzname = UTTCGen_Reread(vo_lib)
+    vo_lib = VoiceOverClipsLibrary(**vo_lib)
+
+    mapping = json.load(open(args.articy_map, "r", encoding="utf-8"))
+    vo = [
+        (mapping.get(clip.ArticyID, "UNUSED"), clip) for clip in vo_lib.clipInformations
+    ]
+    vo = sorted(vo, key=lambda k: k[0])
+    dump = {
+        k: {
+            "ab": info.AssetBundle,
+            "name": info.AssetName,
+            "path": info.PathToClipInProject,
+        }
+        for k, info in vo
+    }
+    with open(args.output, "w", encoding="utf-8") as f:
+        json.dump(dump, f, indent=4, ensure_ascii=False)
+    pass
+
+
+if __name__ == "__main__":
+    __main__()
--- disco.py
+++ disco.py
@@ -1,4 +1,4 @@
-import os
+import os, argparse, json
 import UnityPy
 from UnityPy.enums import ClassIDType
 from UnityPy.classes import MonoBehaviour
@@ -6,29 +6,6 @@ from tqdm import tqdm
 from collections import defaultdict
 
 MAX_DIALOGUE = 10000
-
-ROOT = r"C:\Program Files (x86)\Steam\steamapps\common\Disco Elysium"
-LOCKIT = r"disco_Data\StreamingAssets\AssetBundles\Windows\lockits"
-DIALOG = r"disco_Data\StreamingAssets\aa\StandaloneWindows64\dialoguebundle_assets_all_3472cb598f88f38eef12cdb3aa5fdc80.bundle"
-env = UnityPy.Environment()
-print("loading environment")
-env.load_folder(os.path.join(ROOT, LOCKIT))
-env.load_file(os.path.join(ROOT, DIALOG))
-print("loading assets")
-mono = [obj for obj in env.objects if obj.class_id == ClassIDType.MonoBehaviour]
-dialog = dict()
-for obj in tqdm(mono):
-    obj = obj.read()
-    obj: MonoBehaviour
-    if obj.m_Name.startswith("DialoguesLockit") or obj.m_Name.startswith("Disco"):
-        dialog[obj.m_Name] = obj
-    pass
-disco = dialog["Disco Elysium"]
-
-from_fields = lambda obj: {field.title: field.value for field in obj}
-
-actors = {actor.id: from_fields(actor.fields) for actor in disco.actors}
-all_graph = {convo.id: convo.dialogueEntries for convo in disco.conversations}
 
 
 def escape_quotes(s: str):
@@ -42,112 +19,171 @@ def escape_filename(s: str):
     return s
 
 
-lockit = dialog["DialoguesLockitChinese"]
-lang_def = defaultdict(dict)
-DEST = "graphviz/disco-corpus-cn"
-# TODO: Argparse?
-if True:
-    for data in lockit.mSource.mTerms:
-        term, text = data.Term, data.Languages
-        term = term.split("/")
-        if len(term) == 1:
-            cat, aid = "Unknown", term[0]
-        else:
-            cat, aid = term
-        lang_def[aid][cat] = text
+def __main__():
+    parser = argparse.ArgumentParser(description="Process Disco Elysium dialogue files")
+    parser.add_argument(
+        "--root",
+        default=r"C:\Program Files (x86)\Steam\steamapps\common\Disco Elysium",
+        help="Root directory of Disco Elysium installation",
+    )
+    parser.add_argument(
+        "--lockit-path",
+        default=r"disco_Data\StreamingAssets\AssetBundles\Windows\lockits",
+        help="Lockit (translation) bundle path",
+    )
+    parser.add_argument(
+        "--dialog-path",
+        default=r"disco_Data\StreamingAssets\aa\StandaloneWindows64\dialoguebundle_assets_all_3472cb598f88f38eef12cdb3aa5fdc80.bundle",
+        help="dialoguebundle_assets_all bundle path",
+    )
+    parser.add_argument(
+        "--lockit",
+        default="",
+        help="Lockit (translation) to use to generate translated files",
+    )
+    parser.add_argument(
+        "--articy-map",
+        default="articy_mapping.json",
+        help="Where to store Articy ID:ConvoID.DialogueID mapping json",
+    )
+    parser.add_argument("output", help="Output directory")
+    args = parser.parse_args()
+    env = UnityPy.Environment()
 
+    print("Loading environment")
+    env.load_folder(os.path.join(args.root, args.lockit_path))
+    env.load_file(os.path.join(args.root, args.dialog_path))
 
-os.makedirs(DEST, exist_ok=True)
+    print("Loading assets")
+    mono = [obj for obj in env.objects if obj.class_id == ClassIDType.MonoBehaviour]
+    dialog = dict()
+    for obj in tqdm(mono):
+        obj = obj.read()
+        obj: MonoBehaviour
+        if obj.m_Name.startswith("DialoguesLockit") or obj.m_Name.startswith("Disco"):
+            dialog[obj.m_Name] = obj
+        pass
+    disco = dialog["Disco Elysium"]
 
+    from_fields = lambda obj: {field.title: field.value for field in obj}
 
-def process_one(convo_id):
-    convo = disco.conversations[convo_id]
-    convo_fields = from_fields(convo.fields)
-    cid = convo_fields.get("Articy Id", None)
-    ctitle = convo_fields.get("Title", None)
-    cdesc = convo_fields.get("Description", None)
-    if cid and ctitle and cdesc:
-        fdir = ctitle[0].upper()
-        if not fdir.isalpha():
-            fdir = "_"
-        os.makedirs(os.path.join(DEST, fdir), exist_ok=True)
-        fpath = os.path.join(DEST, fdir, f"{escape_filename(ctitle)}_{convo_id}.gv")
-        f = open(fpath, "w", encoding="utf-8")
-        writeline = lambda *a, **k: print(*a, **k, file=f)
+    actors = {actor.id: from_fields(actor.fields) for actor in disco.actors}
 
-        writeline("#", ctitle)
-        writeline("#", "\n#".join(cdesc.split("\n")))
-        writeline("#", "=" * 50)
-        graph = defaultdict(set)
-        entry_is_fork = (
-            lambda entry: from_fields(entry.fields).get("DialogueEntryType", None)
-            == "Fork"
-        )
-        jumpout = set()
-        for entry in convo.dialogueEntries:
-            for outlink in entry.outgoingLinks:
-                if convo_id + 1 != outlink.destinationConversationID:
-                    dest = (
-                        MAX_DIALOGUE * outlink.destinationConversationID
-                        + outlink.destinationDialogueID
-                    )
-                    jumpout.add(dest)
-                    graph[entry.id].add(dest)
-                else:
-                    graph[entry.id].add(outlink.destinationDialogueID)
-        writeline("digraph G {")
-        for entry in convo.dialogueEntries:
-            u = entry.id
-            entry_fields = from_fields(entry.fields)
-            aid = entry_fields.get("Articy Id", None)
-            lang = lang_def.get(aid, None)
-
-            def get_field(name):
-                fallback = entry_fields.get(name, None)
-                if lang:
-                    ret = lang.get(name, fallback)
-                    if ret:
-                        return ret[0]
-                return fallback
-
-            if not entry_is_fork(entry):
-                text = get_field("Dialogue Text")
-                for alt in range(1, 10):
-                    text_alt = get_field(f"Alternate{alt}")
-                    if text_alt:
-                        text += "\\n" + text_alt
-                    else:
-                        break
-                text = text or get_field("Title")
-
-                actor = int(entry_fields.get("Actor", 0))
-                actor = actors.get(actor, {}).get("Name", None)
-                if actor:
-                    text = f"{actor}: {text}"
-                text = escape_quotes(text)
-                if text:
-                    writeline(f'\t  {u} [label="{text}"];')
+    lang_def = defaultdict(dict)
+    # TODO: Argparse?
+    if args.lockit:
+        lockit = dialog.get(args.lockit, None)
+        assert lockit, "Invalid Lockit. Pick from %s" % ",".join(dialog.keys())
+        for data in lockit.mSource.mTerms:
+            term, text = data.Term, data.Languages
+            term = term.split("/")
+            if len(term) == 1:
+                cat, aid = "Unknown", term[0]
             else:
-                title = entry_fields.get("Title", None)
-                title = escape_quotes(title or "")
-                writeline(f'\t  {u} [label="{title}", shape=diamond];')
-        for u in jumpout:
-            dest = u // MAX_DIALOGUE
-            convo = disco.conversations[dest - 1]
-            convo_fields = from_fields(convo.fields)
-            writeline(
-                f'\t  {u} [label="JUMP OUT to {convo_fields.get('Title',dest)}", shape=diamond];'
+                cat, aid = term
+            lang_def[aid][cat] = text
+
+    DEST = args.output
+    os.makedirs(DEST, exist_ok=True)
+
+    articy_mapping = dict()
+
+    def process_one(convo_id):
+        convo = disco.conversations[convo_id]
+        convo_fields = from_fields(convo.fields)
+        cid = convo_fields.get("Articy Id", None)
+        ctitle = convo_fields.get("Title", None)
+        cdesc = convo_fields.get("Description", None)
+        if cid and ctitle and cdesc:
+            fdir = ctitle[0].upper()
+            if not fdir.isalpha():
+                fdir = "_"
+            os.makedirs(os.path.join(DEST, fdir), exist_ok=True)
+            fpath = os.path.join(DEST, fdir, f"{escape_filename(ctitle)}_{convo_id}.gv")
+            f = open(fpath, "w", encoding="utf-8")
+            writeline = lambda *a, **k: print(*a, **k, file=f)
+
+            writeline("#", ctitle)
+            writeline("#", "\n#".join(cdesc.split("\n")))
+            writeline("#", "=" * 50)
+            graph = defaultdict(set)
+            entry_is_fork = (
+                lambda entry: from_fields(entry.fields).get("DialogueEntryType", None)
+                == "Fork"
             )
-        for u, edges in graph.items():
-            for v in edges:
-                writeline(f"\t  {u} -> {v}")
-        writeline("}")
-        writeline()
+            jumpout = set()
+            for entry in convo.dialogueEntries:
+                for outlink in entry.outgoingLinks:
+                    if convo_id + 1 != outlink.destinationConversationID:
+                        dest = (
+                            MAX_DIALOGUE * outlink.destinationConversationID
+                            + outlink.destinationDialogueID
+                        )
+                        jumpout.add(dest)
+                        graph[entry.id].add(dest)
+                    else:
+                        graph[entry.id].add(outlink.destinationDialogueID)
+            writeline("digraph G {")
+            for entry in convo.dialogueEntries:
+                u = entry.id
+                entry_fields = from_fields(entry.fields)
+                aid = entry_fields.get("Articy Id", None)
+                lang = lang_def.get(aid, None)
+
+                def get_field(name):
+                    fallback = entry_fields.get(name, None)
+                    if lang:
+                        ret = lang.get(name, fallback)
+                        if ret:
+                            return ret[0]
+                    return fallback
+
+                if aid:
+                    articy_mapping[aid] = f"{convo.id}.{u}"
+                if not entry_is_fork(entry):
+                    text = get_field("Dialogue Text")
+                    for alt in range(1, 10):
+                        text_alt = get_field(f"Alternate{alt}")
+                        if text_alt:
+                            text += "\\n" + text_alt
+                        else:
+                            break
+                    text = text or get_field("Title")
+
+                    actor = int(entry_fields.get("Actor", 0))
+                    actor = actors.get(actor, {}).get("Name", None)
+                    if actor:
+                        text = f"{actor}: {text}"
+                    text = escape_quotes(text)
+                    if text:
+                        writeline(f'\t  {u} [label="{text}"];')
+                else:
+                    title = entry_fields.get("Title", None)
+                    title = escape_quotes(title or "")
+                    writeline(f'\t  {u} [label="{title}", shape=diamond];')
+            for u in jumpout:
+                dest = u // MAX_DIALOGUE
+                convo = disco.conversations[dest - 1]
+                convo_fields = from_fields(convo.fields)
+                writeline(
+                    f'\t  {u} [label="JUMP OUT to {convo_fields.get('Title',dest)}", shape=diamond];'
+                )
+            for u, edges in graph.items():
+                for v in edges:
+                    writeline(f"\t  {u} -> {v}")
+            writeline("}")
+            writeline()
+
+    from concurrent.futures import ThreadPoolExecutor
+
+    with ThreadPoolExecutor() as executor:
+        for convo_id, convo in tqdm(enumerate(disco.conversations)):
+            # executor.submit(process_one, convo_id)
+            process_one(convo_id)
+
+    with open(args.articy_map, "w", encoding="utf-8") as f:
+        json.dump(articy_mapping, f)
 
 
-from concurrent.futures import ThreadPoolExecutor
-
-with ThreadPoolExecutor() as executor:
-    for convo_id, convo in tqdm(enumerate(disco.conversations)):
-        # executor.submit(process_one, convo_id)
-        process_one(convo_id)
+if __name__ == "__main__":
+    __main__()
